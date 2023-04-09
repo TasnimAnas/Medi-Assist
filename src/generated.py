b72ac8d655e5826ca83f634f68f086c6108bfee4
Tasnim Anas <anasfarazi0151@gmail.com>
2023-04-09 14:12:56 +0530

    Output Data

--- src/generated.py
+++ src/generated.py
@@ -1,16 +1,15 @@
 import json
+# import openai
 import os
 
-import openai
 import streamlit as st
 
-openai.api_key = "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"
-
 # print(req_lst)
-f = open(r"C:\Users\KIIT\Desktop\medi-assist\data\data.json")
+f = open(r"C:\Users\KIIT\medi-assist\data\data.json")
 req_dict = json.load(f)
 req_lst = req_dict["symptoms"]
-possibleCause = ""
+causes = req_dict["causes"] if "causes" in req_dict else "No Data"
+tests = req_dict["tests"] if "tests" in req_dict else "No Data"
 
 
 class GeneratedDetails:
@@ -23,7 +22,7 @@ class GeneratedDetails:
     def app(self):
         st.title('Generated Details')
         # Taking a dummy list at first and then appending to it
-        dummy_list = ["fever", "cough", "migraine"]
+        # dummy_list = ["fever", "cough", "migraine"]
         list_size = len(self.symptoms)
         st.subheader('Symptoms Explanation')
         col1, col2 = st.columns(2)
@@ -54,21 +53,13 @@ class GeneratedDetails:
                     st.success("Symptom Deleted")
                     req_lst.remove(symptom)
 
+        # st.write('required output')
+        json.dumps(req_lst)
+        # json.dumps(causes)
+        # json.dumps(tests)
 
-completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": f"What is the possible cause of {req_lst}? Just list the possible causes"}
-    ]
-)
-possibleCause = completion.choices[0].message.content
+        st.subheader('Possible Causes')
+        st.write(causes)
 
-
-# st.write('required output')
-json.dumps(req_lst)
-
-st.subheader('Possible Causes')
-st.write(possibleCause)
-
-st.subheader('Tests')
-st.write('output comes here')
+        st.subheader('Tests')
+        st.write(tests)
--- src/input.py
+++ src/input.py
@@ -1,5 +1,10 @@
-import streamlit as st
 import json
+
+import openai
+import streamlit as st
+
+openai.api_key = "KEY"
+
 
 class InputDetails:
     def __init__(self) -> None:
@@ -9,39 +14,46 @@ class InputDetails:
         self.age = None
         self.weight = None
         self.inputJson = None
-        
+
     def update_json(self, key, data):
         with open('data/data.json', 'r') as f:
             self.inputJson = json.load(f)
         self.inputJson[key] = data
         with open('data/data.json', 'w') as f:
             json.dump(self.inputJson, f)
-        
+
     def app(self):
         st.title('Enter Details')
-        
+
         st.subheader('Enter Patient History')
         col1, col2 = st.columns(2)
         with col1:
             self.name = st.text_input('Name')
-            if self.name: self.update_json('name', self.name)
-            self.gender = st.selectbox('Gender', ('Female', 'Male', 'Non-Binary', 'I do not wish to answer'))
-            if self.gender: self.update_json('gender', self.gender)
-        
+            if self.name:
+                self.update_json('name', self.name)
+            self.gender = st.selectbox(
+                'Gender', ('Female', 'Male', 'Non-Binary', 'I do not wish to answer'))
+            if self.gender:
+                self.update_json('gender', self.gender)
+
         with col2:
             self.age = st.text_input('Age')
-            if self.age: self.update_json('age', self.age)
+            if self.age:
+                self.update_json('age', self.age)
             self.weight = st.text_input('Weight')
-            if self.weight: self.update_json('weight', self.weight)
-        
+            if self.weight:
+                self.update_json('weight', self.weight)
+
         self.med_history = st.text_input('Medical History')
-        if self.med_history: self.update_json('med_history', self.med_history)
-        
+        if self.med_history:
+            self.update_json('med_history', self.med_history)
+
         col1, col2 = st.columns(2)
-        
+
         with col1:
             st.subheader("Enter Symptoms:")
-            input = st.text_input('Enter Symptoms:', label_visibility='collapsed')
+            input = st.text_input(
+                'Enter Symptoms:', label_visibility='collapsed')
             if st.button('Add'):
                 if input != "":
                     with open('data/data.json', 'r') as f:
@@ -56,9 +68,26 @@ class InputDetails:
                 self.symptoms = json.load(f)["symptoms"]
             for s in self.symptoms:
                 st.text("> " + s)
-            
+
             if len(self.symptoms) > 0:
                 if st.button('Clear'):
                     with open('data/data.json', 'w') as f:
                         json.dump({"symptoms": []}, f)
                     st.experimental_rerun()
+
+        if st.button('Generate Report'):
+            completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "user", "content": f" A {self.age} years old {self.gender} with a weight {self.weight} is having {self.symptoms}. The patient has a medical history of {self.med_history}. What is the possible cause of these symptoms? Just list the possible causes"}
+                ]
+            )
+            self.update_json('causes', completion.choices[0].message.content)
+
+            completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "user", "content": f" A {self.age} years old {self.gender} with a weight {self.weight} is having {self.symptoms}. The patient has a medical history of {self.med_history}. What medical diagnosis test should be suggested? Just list the possible tests"}
+                ]
+            )
+            self.update_json('tests', completion.choices[0].message.content)
